4703ca4fcc0905ce372731a89832d63411720723
Mrsix29 <michaelzongyao@gmail.com>
2020-06-07 13:25:27 -0600

    Fifth Version

--- src/com/edmonton/fatherdemo/FatherDemo.java
+++ src/com/edmonton/fatherdemo/FatherDemo.java
@@ -3,6 +3,7 @@ package com.edmonton.fatherdemo;
 public class FatherDemo {
     public static void main(String[] args) {
         int number = 1;
+        String str = "";
         int number2 = 3;
     }
 }
